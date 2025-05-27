f91e8f771037581ab21344d518780df357a4e5d4
Davis Roldan <davisrdln@gmail.com>
2025-05-27 02:17:44 -0600

    fix: round robin

--- algorithms/round_robin.py
+++ algorithms/round_robin.py
@@ -2,43 +2,54 @@ def round_robin_scheduler(processes, quantum):
     from collections import deque
     from copy import deepcopy
 
-    processes = sorted(processes, key=lambda p: p['arrival_time'])
-    queue = deque()
-    remaining = {p['pid']: p['burst_time'] for p in processes}
-    arrival_times = {p['pid']: p['arrival_time'] for p in processes}
-    process_map = {p['pid']: p for p in processes}
-
+    processes = deepcopy(processes)
     time = 0
+    queue = deque()
     timeline = []
-    waiting_times = {}
-    last_executed = {}
+    remaining_bt = {p['pid']: p['burst_time'] for p in processes}
+    arrival_times = {p['pid']: p['arrival_time'] for p in processes}
+    finish_times = {}
+    arrived = set()
 
-    i = 0
-    while remaining:
-        while i < len(processes) and processes[i]['arrival_time'] <= time:
-            queue.append(processes[i]['pid'])
-            i += 1
+    while len(finish_times) < len(processes):
+        # Insertar procesos que llegan en este ciclo
+        for p in processes:
+            if p['arrival_time'] <= time and p['pid'] not in arrived:
+                queue.append(p['pid'])
+                arrived.add(p['pid'])
 
         if not queue:
             time += 1
             continue
 
         pid = queue.popleft()
-        bt = remaining[pid]
-        exec_time = min(quantum, bt)
+        exec_time = min(quantum, remaining_bt[pid])
+        timeline.append({
+            'pid': pid,
+            'start': time,
+            'end': time + exec_time
+        })
 
-        timeline.append({'pid': pid, 'start': time, 'end': time + exec_time})
         time += exec_time
-        remaining[pid] -= exec_time
+        remaining_bt[pid] -= exec_time
 
-        while i < len(processes) and processes[i]['arrival_time'] <= time:
-            queue.append(processes[i]['pid'])
-            i += 1
+        # Revisar si otros procesos llegan durante este tiempo
+        for p in processes:
+            if p['arrival_time'] > time - exec_time and p['arrival_time'] <= time and p['pid'] not in arrived:
+                queue.append(p['pid'])
+                arrived.add(p['pid'])
 
-        if remaining[pid] > 0:
+        if remaining_bt[pid] > 0:
             queue.append(pid)
         else:
-            waiting_times[pid] = time - arrival_times[pid] - process_map[pid]['burst_time']
+            finish_times[pid] = time
 
-    avg_waiting_time = sum(waiting_times.values()) / len(waiting_times)
+    # Calcular tiempo de espera: Finish - Arrival - Burst
+    waiting_times = []
+    for p in processes:
+        pid = p['pid']
+        wt = finish_times[pid] - p['arrival_time'] - p['burst_time']
+        waiting_times.append(wt)
+
+    avg_waiting_time = sum(waiting_times) / len(waiting_times)
     return {'timeline': timeline, 'avg_waiting_time': avg_waiting_time}
